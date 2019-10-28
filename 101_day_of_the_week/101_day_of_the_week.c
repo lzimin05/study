963706e3009dbd89437b8e7f990dc6043b199523
Leonid <lzimin05@mail.ru>
2019-10-28 19:05:44 +0300

    Change 101 program

--- 101_day_of_the_week/101_day_of_the_week.c
+++ 101_day_of_the_week/101_day_of_the_week.c
@@ -8,15 +8,37 @@ int main(int argc, char** argv) {
 	
 	if (a == 6) {
 		printf("Saturday");
-	
+		return 0;
 	}
 	if (a == 7) {
 		printf("Sunday");
+		return 0;
 	}
 	if (a <= 5 && a >= 1) {
 		printf("Workday");
 	} else {
 		printf("ERROR");
 	}
+	
+	printf("\n-----------------\n");
+	switch (a) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			printf("Workday");
+			break;
+		case 6:
+			printf("Saturday");
+			break;
+		case 7:
+			printf("Sunday");
+			break;
+		default:
+			printf("ERROR");
+			break;
+	}
+	
 	return 0;
 }
