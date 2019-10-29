4b65e80b374f5f5454659fa1d650fed62c07dc6e
Leonid <lzimin05@mail.ru>
2019-10-29 11:29:14 +0300

    Add program 105

---
+++ 105_material/105_material.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	
+	printf("-----------------\nJalousie:\n");
+	printf("Enter the widht (cm) -> ");
+	float a;
+	scanf("%f", &a);
+	
+	printf("-----------------\nEnter the height (cm) -> ");
+	float h;
+	scanf("%f", &h);
+	
+	printf("-----------------\nMaterial:\n1 - plastic\n2 - textile\n3 - aluminum\n");
+	printf("your choice (1 - 3) -> ");
+	int c;
+	scanf("%i", &c);
+	float cena;
+	switch (c) {
+		case 1: 
+			cena = 200;
+			break;
+		case 2: 
+			cena = 250;
+			break;
+		case 3: 
+			cena = 350;
+			break;
+		default:
+			printf("ERROR");
+			break;
+			return 1;
+	}
+	float s = (a * h) / 10000;
+	float sum = s * cena;
+	printf("\n-----------------\nprice per square meter - %.2f", cena);
+	printf("\n-----------------\narea - %.2f", s);
+	printf("\n-----------------\nto pay - %.2f roubles", sum);
+	return 0;
+}
