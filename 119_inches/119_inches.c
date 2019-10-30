d4dfdf6a0688986c2fa8a8c265bd97283f4a63a9
Leonid <lzimin05@mail.ru>
2019-10-30 13:59:14 +0300

    Change program 119

--- 119_inches/119_inches.c
+++ 119_inches/119_inches.c
@@ -13,20 +13,19 @@ int main(int argc, char** argv) {
 	printf("Enter step -> ");
 	float dt;
 	scanf("%f", &dt);
-	printf("\n----------------------------------\n");
+	printf("\n-------------------------\n");
 
 	float n = (t2 - t1) / dt + 1;
 	
-	float inches, milimetrs, santimetrs;
+	float inches, millimeters, centimeters;
 	
-	printf(" Inches   Milimetrs   Santimeters\n----------------------------------\n");
+	printf(" Inches   Millimeters   centimeters\n-------------------------\n");
 	
 	for (int i = 0; i < n; i++) {
-	milimetrs = 254 * inches;
-	santimetrs = 2.54f * inches;
-	printf("%7.3f    %7.3f    %7.3f\n", inches, milimetrs, santimetrs);
-	inches = inches + dt;
-	
+		millimeters = 254 * inches;
+		centimeters = 2.54f * inches;
+		printf("%7.3f    %7.3f    %7.3f\n", inches, millimeters, centimeters);
+		inches = inches + dt;
 	}
 	
 	return 0;
