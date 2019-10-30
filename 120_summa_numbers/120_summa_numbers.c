4206d5b0a092b83b6f6071443b59fa4d1c28c36e
Leonid <lzimin05@mail.ru>
2019-10-30 14:14:49 +0300

    Add program 120

---
+++ 120_summa_numbers/120_summa_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	
+	int a;
+	printf("Enter number -> ");
+	scanf("%i", &a);
+	
+	int summ = 0;
+	for (int i = 0; i <= a; i++) {
+	summ+= i;
+	
+	}
+	printf("\n---------------\n%i",summ);
+	return 0;
+}
