fb36d92dbd1d486e600bdc906f708432f72faea1
Leonid <lzimin05@mail.ru>
2019-10-31 13:18:50 +0300

    Add program 133

---
+++ 133_module/133_module.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	
+	printf("module:\n");
+	int a = 4;
+	float x = -4;
+	for (int i = 1; i < 18; i++) {
+		if (x < 0) {
+			float y = x * -1;
+			printf("%3.2f %3.2f\n", x, y); 
+		} else {
+			float y = x;
+			printf("+%4.2f %4.2f\n", x, y);
+		}
+		x = x + 0.5f;
+	}
+	
+	return 0;
+}
